35e92356301a79137685421aad40a4653b4613b6
Marcin <marcin_bf3@interia.pl>
2024-12-12 13:59:39 +0100

    Zrobienie menu z przyciskami

--- PWRKapp/urls.py
+++ PWRKapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.StartingPageView.as_view(), name="starting-page")
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("new-entry", views.NewEntryView.as_view(), name="new-entry"),
+    path("all-entries", views.AllEntriesView.as_view(), name="all-entries"),
+    path("entry-handling", views.EntryHandlingView.as_view(), name="entry-handling")
+
 ]
--- PWRKapp/views.py
+++ PWRKapp/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
 class StartingPageView(View):
-    pass
+    def get(self, request):
+        return HttpResponseRedirect(reverse("new-entry"))
+
+
+class NewEntryView(View):
+    def get(self, request):
+        # welcome_user = request.META.get('REMOTE_USER')
+        test_list = []
+        for i in range(1, 30000):
+            test_list.append(i)
+        context = {
+            "nav_button_active": 1,
+            "comp_list": test_list,
+            # "welcome_user": welcome_user
+        }
+        return render(request, "PWRKapp/new_entry.html", context)
+
+
+class AllEntriesView(View):
+    def get(self, request):
+        context = {
+            "nav_button_active": 2
+        }
+        return render(request, "PWRKapp/new_entry.html", context)
+
+
+class EntryHandlingView(View):
+    def get(self, request):
+        context = {
+            "nav_button_active": 3
+        }
+        return render(request, "PWRKapp/new_entry.html", context)

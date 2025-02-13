23e6b3fc1f2424f96a939c677345904da57e057a
Marcin <marcin_bf3@interia.pl>
2025-02-13 20:54:44 +0100

    Dodanie obslugi uzytkownika w adminie

--- PWRKapp/admin.py
+++ PWRKapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Employee, Entry
 # Register your models here.
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+
+    list_filter = ("username", "admin_accesss",)
+    list_display = ("username", "admin_accesss",)
+   # prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Employee, EmployeeAdmin)
